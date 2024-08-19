74a3c096832ec9332f52a059fd434ac5c2cec9dc
yn030727 <2601098388@qq.com>
2024-08-19 11:14:43 +0800

    update ViewModel

---
+++ NingAndroid/ViewModel.java
@@ -0,0 +1,5 @@
+package NingAndroid;
+
+public class ViewModel {
+
+}
--- NingCode1/Test.java
+++ NingCode1/Test.java
@@ -1,22 +1,11 @@
 package NingCode1;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.Scanner;
-import java.util.Set;
-import java.util.StringTokenizer;
-import java.util.TreeMap;
+import java.util.*;
 
+//笔试特供
 public class Test {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
