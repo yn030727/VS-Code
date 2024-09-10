c1f2cd5986eb2bc11c440ad970aed54e60623a47
yn030727 <2601098388@qq.com>
2024-09-10 16:50:51 +0800

    LeetCode Code1

--- NingCode1/Test.java
+++ NingCode1/Test.java
@@ -1,51 +1,9 @@
 package NingCode1;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.util.*;
 
-//笔试特供
-public class Test {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
-        
-        int n = Integer.parseInt(tokenizer.nextToken());
-        int q = Integer.parseInt(tokenizer.nextToken());
-        
-        long[] a = new long[n + 1];
-        tokenizer = new StringTokenizer(reader.readLine());
-        for (int i = 1; i <= n; i++) {
-            a[i] = Long.parseLong(tokenizer.nextToken());
-        }
-        
-        long[] prefixSum = new long[n + 1];
-        for (int i = 1; i <= n; i++) {
-            prefixSum[i] = prefixSum[i - 1] + a[i];
-        }
-        
-        PrintWriter writer = new PrintWriter(System.out);
-        for (int i = 0; i < q; i++) {
-            tokenizer = new StringTokenizer(reader.readLine());
-            String c = tokenizer.nextToken();
-            int x = Integer.parseInt(tokenizer.nextToken());
-            
-            if (c.equals("L")) {
-                long sum = prefixSum[x % n] - prefixSum[0];
-                writer.println(sum);
-            } else if (c.equals("R")) {
-                long sum = prefixSum[n] - prefixSum[n - (x % n) - 1];
-                writer.println(sum);
-            }
-        }
-        
-        writer.flush();
-      
-    }
-
-}
-
-class ListNode{
+//LeetCode：字符串和数组
+public class TestStringAndArray {
+    public static void main(String[] args) {
+      System.out.print(1 ^ 0);  
+    } 
 
 }
