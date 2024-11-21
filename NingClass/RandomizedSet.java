c6abef3feb20b4a1d3b5b37ab27f453ff56f1b2f
yn030727 <2601098388@qq.com>
2024-11-21 18:10:38 +0800

    Code1121[2]

---
+++ NingClass/RandomizedSet.java
@@ -0,0 +1,30 @@
+package NingClass;
+
+/*
+ * 
+ */
+public class RandomizedSet {
+    public RandomizedSet() {
+        
+    }
+    
+    public boolean insert(int val) {
+        return false;
+    }
+    
+    public boolean remove(int val) {
+        return false;
+        
+    }
+    
+    public int getRandom() {
+        return 0;
+    }
+}
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
